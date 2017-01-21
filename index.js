e0aca2642a50701343d1282a036dbea15b978d4b
jonsavagek36 <jonsavagek36@gmail.com>
2017-01-21 16:40:48 -0500

    fixed for heroku

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ let chat = require('./chatapp');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-let http = require('http').createServer(app).listen(3000);
+let port = process.env.PORT || 5000;
+let http = require('http').createServer(app).listen(port);
 let io = require('socket.io')(http);
 
 io.sockets.on('connection', function(socket) {
